5e9a17d5a0d3bbc046fc43c0d32bd4ac146ebafa
Buhanghang <jay.s.buhanghang@accenture.com>
2026-09-12 19:36:05 +0800

    pic hover edit

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
    		 <Navbar />
 
 			<div className="lg:ml-60">
-				<div className="mx-auto max-w-4xl px-6 lg:px-8">
+				<div className="mx-auto max-w-5xl px-6 lg:px-8">
 					<Hero />
 					<About />
 					<Skills />
